30e352d3799689becaa3c6fb081da4e09f5a9757
Shahaf Bahar <shahaf.bahar@k2view.com>
2022-05-03 10:18:42 +0300

    my commit message

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ const port = process.env.PORT|| 3002;
 app.use(bodyParser.json());
 app.use(cors());
 
+
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+
+
 const { exec } = require('child_process');
  exec('dir', {'shell':'powershell.exe'}, (error, stdout, stderr)=> {
     // do whatever with stdout
