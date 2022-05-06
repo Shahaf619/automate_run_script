59b111068e9258f4b621bfe41805e037b9562dd4
Shahaf Bahar <shahaf.bahar@k2view.com>
2022-05-06 21:09:40 +0300

    Deploy

--- server/server.js
+++ server/server.js
@@ -13,18 +13,18 @@ app.use(cors());
 // When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-const { exec } = require('child_process');
-exec('dir',  (error, stdout, stderr)=> 
- {
+//const { exec } = require('child_process');
+//exec('dir', {'shell':'powershell.exe'}, (error, stdout, stderr)=> 
+ //{
     // do whatever with stdout 
-    console.log(stdout);
+    console.log("stdout");
     app.get('/api', (req, res) => 
     {
         console.log("here is api")
-        res.send({data : stdout})
+        res.send({data : "stdout"})
         //res.json({ message: "Hello from server!" });
     })
-})
+//})
 
 
 // Create API for POST data
