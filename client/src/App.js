5d5f34eebb76e34f0794c2e5b16b86ee7b35fa96
Shahaf Bahar <shahaf.bahar@k2view.com>
2022-05-06 21:27:55 +0300

    Deploy2

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App()
   // Get the shell output data
   function getapi() 
   {//http://localhost:3002
-    fetch("/api", { 
+    fetch("/api2", { 
       method: "GET",
       headers: { "Content-Type": "application/json"} })
     .then((res) => res.json())
